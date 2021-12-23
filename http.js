a92c05a5fb45455ed464256553cd35f36bb8e0ad
viktor poliushko <viktor.poliushko@workrise.com>
2021-12-23 03:29:36 +0200

    duplex streams

--- http.js
+++ http.js
@@ -1,7 +1,59 @@
 const {createServer} = require('http');
-const {createReadStream} = require('fs');
-const fileName = './testFile.png';
+const {stat, createReadStream, createWriteStream} = require('fs');
+const {promisify} = require('util');
+const multiparty = require('multiparty');
+
+const fileName = './123.mp4';
+const fileInfo = promisify(stat);
+
+const sendOriginalVideo = async (req, res) => {
+  const {size} = await fileInfo(fileName);
+  const range = req.headers.range;
+  if(range) {
+    let [start, end] = range.replace(/bytes=/, '').split('-');
+    start = parseInt(start, 10);
+    end = end ? parseInt(end, 10) : size - 1;
+
+    res.writeHead(206, {
+      'Content-Range': `bytes ${start}-${end}/${size}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': (start-end) + 1,
+      'Content-Type': 'video/mp4'
+    });
+    
+    createReadStream(fileName, {start, end}).pipe(res);
+  } else {
+    res.writeHead(200, {
+      'Content-Type': size,
+      'Content-Type': 'video/mp4'
+    });
+
+    createReadStream(fileName).pipe(res);
+  }
+};
 
 createServer((req, res) => {
-
-}).listen(3000, () => console.log('server is running on port 3000'));
+  if(req.method === "POST") {
+    let form = new multiparty.Form();
+    form.on('part', (part) => {
+      part
+        .pipe(createWriteStream(`./copy/${part.filename}`))
+        .on('close', () => {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.end(`<h1>File uploaded: ${part.filename}</h1>`)
+        });
+    });
+    form.parse(req);
+  }
+  else if(req.url === "/original") {
+    sendOriginalVideo(req, res);
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(`
+      <form enctype="multipart/form-data" method="POST" action="/">
+        <input type="file" name="upload-file">
+        <button>Upload File</button>
+      </form>
+    `);
+  }
+}).listen(5050, () => console.log('server is running on port 5050'));
